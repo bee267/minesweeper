973ab1163194d910d1f88accd62ad289e35e4ccb
PhuongDo1801 <92222210+PhuongDo1801@users.noreply.github.com>
2021-11-05 02:01:32 +0700

    Update functions.h

--- functions.h
+++ functions.h
@@ -5,3 +5,6 @@ void deleteArray();
 void constructor(short R, short C, short B);
 void drawTable();
 void drawPiece(short x, short y, short color);
+void createBomb(); // tạo bom ngãu nhiên
+void exportBomb(); // xuất bom
+void xuLySuKien();
